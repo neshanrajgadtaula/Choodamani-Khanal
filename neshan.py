931668a950d178977be328b56f32325f70198a1c
neshanrajgadtaula <nesshan10@gmail.com>
2025-10-07 01:11:28 +0545

    two sums

---
+++ neshan.py
@@ -0,0 +1,9 @@
+def generate_two_sums(a, b, c, d):
+    sum1 = a + b
+    sum2 = c + d
+    return sum1, sum2
+
+# Example usage
+result1, result2 = generate_two_sums(1, 2, 3, 4)
+print("First sum:", result1)
+print("Second sum:", result2)
